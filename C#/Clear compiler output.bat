e2f3c26c8e00c03f3359c58e64148dde763554a7
Steffen Liersch <S.Liersch@gmx.de>
2020-10-15 17:47:03 +0200

    Minor change

--- C#/Clear compiler output.bat
+++ C#/Clear compiler output.bat
@@ -3,14 +3,6 @@
 :: Copyright © 2020 Dipl.-Ing. (BA) Steffen Liersch
 :: All rights reserved.
 ::
-:: Steffen Liersch
-:: Robert-Schumann-Straße 1
-:: 08289 Schneeberg
-:: Germany
-::
-:: Phone: +49-3772-38 28 08
-:: E-Mail: S.Liersch@gmx.de
-::
 ::----------------------------------------------------------------------------
 
 @echo off
