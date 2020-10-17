21f34971598884f061d9c62caa6806e5694de81b
Steffen Liersch <S.Liersch@gmx.de>
2020-10-17 12:26:02 +0200

    Docker composition for MariaDB added

---
+++ Docker/Linux Scenarios/mariadb/Setup.bat
@@ -0,0 +1,46 @@
+::----------------------------------------------------------------------------
+::
+:: Copyright © 2020 Dipl.-Ing. (BA) Steffen Liersch
+:: All rights reserved.
+::
+::----------------------------------------------------------------------------
+
+@echo off
+pushd "%~dp0"
+
+where /Q docker-compose
+if not %ERRORLEVEL%==0 (echo The required "docker-compose" utility is not installed. & goto error)
+docker-compose ps
+echo.
+
+echo [1] Start composition
+echo [2] Stop composition
+echo [E] Exit
+echo.
+choice /N /M "Choice: " /C 12EQX
+echo.
+if %ERRORLEVEL%==1 goto start
+if %ERRORLEVEL%==2 goto stop
+goto end
+
+:start
+docker-compose down -t 0
+if not %ERRORLEVEL%==0 goto error
+docker-compose up --build
+if not %ERRORLEVEL%==0 goto error
+goto end
+
+:stop
+docker-compose down -t 0
+if not %ERRORLEVEL%==0 goto error
+goto end
+
+:end
+popd
+goto :eof
+
+:error
+echo.
+pause
+popd
+exit
